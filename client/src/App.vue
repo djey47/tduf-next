6a6eb1f3ecb531766bfc29fbac8d461772511358
djey47 <djey47@github.com>
2020-07-01 15:11:11 +0200

    refactor(intro): create Intro component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,21 +1,21 @@
 <template>
   <div id="app">
-    <p class="app__intro">
-      <img class="app__introLogo" alt="Vue logo" src="./assets/logo.png">
-    </p>
+    <Intro />
     <Home v-bind:conf="this.conf" />
   </div>
 </template>
 
 <script>
 import clientConfig from './config/tduf-next.client.config.yaml';
-import Home from './components/Home.vue';
+import Home from './components/pages/Home.vue';
+import Intro from './components/molecules/Intro.vue';
 import { get } from './helpers/rest-client';
-
+ 
 export default {
     name: 'App',
     components: {
-        Home
+        Intro,
+        Home,
     },
     data() {
         return {
@@ -47,12 +47,5 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-}
-.app__intro {
-  text-align: left;
-}
-.app__introLogo {
-  width: 64px;
-  height: auto;
 }
 </style>
---
+++ client/src/components/molecules/Intro.vue
@@ -0,0 +1,21 @@
+<template>
+    <p class="Intro">
+      <img class="Intro__logo" alt="Vue logo" src="../../assets/logo.png">
+    </p>    
+</template>
+
+<script>
+export default {
+    name: 'Intro',
+};
+</script>
+
+<style scoped>
+.Intro {
+  text-align: left;
+}
+.Intro__logo {
+  width: 64px;
+  height: auto;
+}
+</style>
