4dd72fd9d11dbb4f0820cb045e1e1c32276d49b9
djey47 <djey47@github.com>
2020-07-01 16:08:11 +0200

    feature(intro): enhance styling

--- client/src/components/molecules/Intro.vue
+++ client/src/components/molecules/Intro.vue
@@ -1,6 +1,7 @@
 <template>
     <p class="Intro">
       <img class="Intro__logo" alt="Vue logo" src="../../assets/logo.png">
+      <span class="Intro__title">TDUF.next</span>
     </p>    
 </template>
 
@@ -13,9 +14,21 @@ export default {
 <style scoped>
 .Intro {
   text-align: left;
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  color: whitesmoke;
+  background-color: darkslateblue;
 }
+
+.Intro__title {
+  flex: 1;
+  font-size: 2.6rem;
+  text-align: center;
+}
+
 .Intro__logo {
-  width: 64px;
-  height: auto;
+  height: 64px;
+  flex: 0 0 64px;
 }
 </style>
