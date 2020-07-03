6d1f93946ed35775a98bcb0929d20eb193189c2f
djey47 <djey47@github.com>
2020-07-03 15:55:48 +0200

    chore(package): zip for 3 platforms

--- server/scripts/package-full.js
+++ server/scripts/package-full.js
@@ -9,51 +9,97 @@ const rimraf = require('rimraf');
 const packageInfo = require(path.join(appRootDir, 'package.json'));
 const { info, error } = require('../src/helpers/console');
 
+async function zip(platform, packPath) {
+    return new Promise((resolve, reject) => {
+        const targetExecutable = 'tduf.next';
+        const releasePath = path.resolve(appRootDir, '../releases');
+        const zipSettings = {
+            linux: {
+                osLabel: ':penguin: Linux',
+                executable: 'index-linux',
+            },
+            macos: {
+                osLabel: ':apple: Mac OS',
+                executable: 'index-macos',
+            },
+            windows: {
+                osLabel: '🏳️  Windows',
+                executable: 'index-win.exe',
+            },
+        };
+        const currentSettings = zipSettings[platform];
+        if (!currentSettings) {
+            reject(`No zip configuration available for platform: ${platform}`);
+        }
+
+        if (!fs.existsSync(releasePath)) {
+            info(':new: Creating releases target...', { releasePath });
+            fs.mkdirSync(releasePath);
+        }  
+
+        const { executable, osLabel } = currentSettings;
+        const sourceExecutablePath = path.join(packPath, executable);
+
+        const releaseArchivePath = path.join(releasePath, `tduf-next-${packageInfo.version}-${platform}64.zip`);
+        info(`:package: Zipping release (${osLabel})...`, { releaseArchivePath });
+        const outputStream = fs.createWriteStream(releaseArchivePath);
+
+        /**
+         * zlib has 10 compression levels (0-9).
+         * Different levels have different compression performance in terms of compression ratio and speed .
+         * Level 0 means no compression, zlib will output the original data
+         */
+        const releaseArchive = archiver('zip', {
+        zlib: { level: 9 }, // Sets the compression level.
+        });
+        // listen for all archive data to be written
+        // 'close' event is fired only when a file descriptor is involved
+        outputStream.on('close', function() {
+            info(':heavy_check_mark:  Archive created', {
+                 platform,
+                 releaseArchivePath,
+                 totalBytes: releaseArchive.pointer(),
+                 executable,
+             });
+            resolve();
+        });
+
+        releaseArchive.pipe(outputStream);
+        releaseArchive.file(sourceExecutablePath, { name: targetExecutable });
+        releaseArchive.file(path.join(packPath, 'README.txt'), { name: 'README.txt' });
+        releaseArchive.finalize();
+    });
+}
+
 async function packageFull() {
     info(':gear:  TDUF.next FULL packaging');
 
     const packPath = path.join(appRootDir, 'pack');
-    info('🧹 Cleaning packaging target...', { packPath });
-    rimraf(path.join(packPath, "*"), () => {} );
+    if (!fs.existsSync(packPath)) {
+        info(':new: Creating packaging target...', { packPath });
+        fs.mkdirSync(packPath);
+    } else {
+        info('🧹 Cleaning packaging target...', { packPath });
+        rimraf(path.join(packPath, "*"), () => {} );
+    }
     
     const clientPath = path.resolve(appRootDir, '../client');
     info(':globe_with_meridians: Processing webapp...', { clientPath });
     childProcess.execSync('npm run build', { cwd: clientPath });
 
-    const serverPath = path.resolve(appRootDir, '../server');
-    info(':robot_face: Processing server (All platforms)...', serverPath);
-    const sourceExecutablePath = path.join(packPath, 'index-linux');
-    const targetExecutable = 'tduf.next';
+    const serverPath = path.resolve(appRootDir);
+    info(':robot_face: Processing server (All platforms)...', { serverPath });
+
     await pkg.exec([/*'--debug',*/ '-c', './scripts/pkg.json', 'src/index.js', '--out-path', packPath ]);
 
     info(':open_file_folder: Processing assets (README and co)...');
     const assetsPath = path.join(appRootDir, 'dist');
-    await fs.promises.copyFile(path.join(assetsPath, 'README.txt'), path.join(packPath, 'README.txt'));    
+    await fs.promises.copyFile(path.join(assetsPath, 'README.txt'), path.join(packPath, 'README.txt'));
     
-    // TODO handle all platforms
-    info(':package: Zipping release server (:penguin: Linux)...');
-    const releaseArchivePath = path.join(appRootDir, 'releases', `tduf-next-${packageInfo.version}-linux64.zip`);
-    const outputStream = fs.createWriteStream(releaseArchivePath);
+    const zipPromises = ['linux', 'macos', 'windows'].map(p => zip(p, packPath));  
+    await Promise.all(zipPromises);
 
-    /**
-     * zlib has 10 compression levels (0-9).
-     * Different levels have different compression performance in terms of compression ratio and speed .
-     * Level 0 means no compression, zlib will output the original data
-     */
-    const releaseArchive = archiver('zip', {
-      zlib: { level: 9 }, // Sets the compression level.
-    });
-    // listen for all archive data to be written
-    // 'close' event is fired only when a file descriptor is involved
-    outputStream.on('close', function() {
-        info(':heavy_check_mark:  Archive created', { platform: 'linux', releaseArchivePath, totalBytes: releaseArchive.pointer() });
-        info(':beer: All done!');
-    });
-
-    releaseArchive.pipe(outputStream);
-    releaseArchive.file(sourceExecutablePath, { name: targetExecutable });
-    releaseArchive.file(path.join(packPath, 'README.txt'), { name: 'README.txt' });
-    releaseArchive.finalize();
+    info(':beer: All done!');
 }
 
 try {
